1e33e12ca3ca04c77d0307b270e9ace634f5e30f
Smukihub <60901189+Smukihub@users.noreply.github.com>
2020-06-08 03:02:41 -0500

    Add files via upload

---
+++ app/Http/Controllers/API/CategoryController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Category;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $cat = new Category();
+        $cat->nombre="Quimicos";
+        $cat->slug="quimicos";
+        $cat->descripcion="Productos quimicos";
+        $cat->save();
+        return $cat;
+     
+        
+    }
+    /*
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($slug)
+    {
+        if (Category::where('slug',$slug)->first()){
+        return 'Slug existe';
+        }
+        else{
+            return 'Slug Disponible';
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
